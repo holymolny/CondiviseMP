06327971023bee4e6c3241bbd5951706770cc7fa
Eneriilop <i.poli5@studenti.unipi.it>
2024-04-03 18:08:51 +0200

    Co-authored-by: Mihnea Sever Molnar <holymolny@users.noreply.github.com>

--- gui.py
+++ gui.py
@@ -16,36 +16,93 @@ e salvare su file il nuovo stato del Bancomat;
 L’aspetto generale dell’interfaccia grafica `e lasciato alla creativit`a dello studente.
 """
 
-
-
-
 from bancomat import *
+from utenti import *
 import tkinter as tk
 from tkinter import Tk
 from tkinter import messagebox
 
 class BancomatApp():
-    def __init__(self):
-        self.title("Bancomat Molpol")
-        self.geometry("300x300")
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Bancomat Molpol")
+        #self.root.geometry("300x300")
+        #Utilizzo di grid per posizionare il frame principale
+        self.frame = tk.Frame(self.root, relief=tk.RAISED, borderwidth=1)
+        self.frame.grid(row=0, column=0, sticky="nsew")
 
-        bancomat = Bancomat({},0,0)
-        bancomat.carica_da_file()
+        btnCarica = tk.Button(
+            text="Carica File", 
+            width=20,
+            height=5,
+            bg="grey",
+            fg="black",
+            #command=self.read_from_file(),
+        )
+
+        #Label, Entry e rispettivi grid
+        #Archivio da cercare per caricarlo o salvarlo 
+        self.cerca = tk.Label(self.frame, text="Nome archivio:")
+        self.cerca.grid(row=2, column=0, padx=2, pady=2, sticky="w")
+        #Campo di ricerca dell'archivio
+        self.entry_cerca_arc = tk.Entry(self.frame, width=30)
+        self.entry_cerca_arc.grid(row=2, column=1, padx=2, pady=2)
+        #Bottoni associati al campo dove inserire il nome dell'archivio
+        self.btnCarica = tk.Button(self.frame, text="Carica archivio")
+        self.btnCarica.grid(row=2, column=2, padx=2, pady=2)
+        self.btn_salva = tk.Button(self.frame, text="Salva archivio")
+        self.btn_salva.grid(row=3, column=1, padx=2, pady=2)
+
+
+
+        #self.btnCarica.pack()
+        userAdmin, pswAdmin = self.read_from_file("bancomat.txt")
+        systemAdmin = Admin(userAdmin, pswAdmin)
+        bancomat = Bancomat(systemAdmin, 0, 0)
+        bancomat.carica_da_file(userAdmin, pswAdmin, "bancomat.txt")
         print(bancomat)
 
-    #FUNZIONE CHE MI SERVE PER CARICARE IL FILE DATI
-    def carica_file(self):
+
+        #Lista dei bind()
+        self.btnCarica.bind("<Button-1>", self.btn_archivio_premuto)
+        
+
+    #def handle_btnCarica_press(event):
+    def btn_archivio_premuto(self, event):
+        #Quando il bottone è stato premuto:
+        #imposto la variabile a True 
+        self.premuto_var.set(True)
+        #associo l'evento che carica l'archivio
+        self.carica_archivio(event)
+    
+    def carica_archivio(self, evento):
+        #Prendo il nome dell'archivio dalla casella di testo associata
+        nome_archivio = self.entry_cerca_arc.get()
+        #Verifico che il nome dell'archivio sia stato inserito e che sia stato cliccato il bottone per caricare l'archivio
+        if not(nome_archivio and self.premuto_var.get()):
+            #Mostro un messaggio di avvertimento se il nome dell'archivio non è stato inserito nel campo corrispondente
+            messagebox.showwarning("Attenzione", "Caricare il file con l'archivio prima di fare qualunque azione.")
+            return
+        else:
+            #Altrimenti l'archivio viene caricato in base a entry_cerca_arc.get()
+            self.archivio.carica(nome_archivio)
+            #In seguito viene aggiornata la visualizzazione dell'interfaccia con l'intero archivio caricato
+            self.studvar.set(self.archivio)
+         
+    #FUNZIONE CHE LEGGE LE CREDENZIALI DELL'ADMIN DI SISTEMA
+    def read_from_file(self, filename):
         try:
             #carico il file in lettura 
-            with open("bancomat.txt", "r") as file:
-                stato_bancomat = file.read()
-                self.bancomat.carica_stato(stato_bancomat)
-        except FileNotFoundError:
-            messagebox.showerror("Errore", "Non è stato trovato il file bancomat.txt")
-            self.destroy()
+            with open(filename, "r") as file:
+                for riga in file:
+                    riga = riga.strip()
+                    if riga.startswith("Admin:"):
+                        user, psw = riga.split()[1:3]
+                        return user, psw
+        except IOError:
+                #print("Errore nell'apertura del file: " + filename)
+                return False 
 
-
-
-
-app = BancomatApp
-app.mainloop()
+window = tk.Tk()
+BancomatApp(window)
+window.mainloop()
